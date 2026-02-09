f92820dd478b949127750d4e8a62614abcdf7813
Dillon Rich <Knextdr@gmail.com>
2026-02-09 14:43:15 -0600

    Create playback.py

---
+++ ProblemSet0/playback.py
@@ -0,0 +1,8 @@
+def main():
+
+    words = input("Give me some words!: ").replace (" ", "...")
+    return print (words)
+
+if __name__ == "__main__": main()
+
+# implement a program in Python that prompts the user for input and then outputs that same input, replacing each space with ... (i.e., three periods).
