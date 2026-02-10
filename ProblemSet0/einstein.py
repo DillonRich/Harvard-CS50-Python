02b8036916fde0b5e7bcb611835ebfa5f39cf7dc
Dillon Rich <Knextdr@gmail.com>
2026-02-10 15:23:26 -0600

    Add user prompt for mass and energy calculation
    Implement user input for mass and calculate energy in Joules.

--- ProblemSet0/einstein.py
+++ ProblemSet0/einstein.py
@@ -5,6 +5,7 @@ def main():
     E = m * c**2
     print(E)
 # print(f"The mass of {m} Kg is equal to {E} Jules")
+
 if __name__ == "__main__": main()
 
 
@@ -12,3 +13,4 @@ if __name__ == "__main__": main()
 𝐸 =𝑚⁢𝑐2, wherein 𝐸 represents energy (measured in Joules), 𝑚 represents mass (measured in kilograms), and 𝑐 represents the speed of light (measured approximately as 300000000 meters per second), per Albert Einstein et al. Essentially, the formula means that mass and energy are equivalent.
 
 In a file called einstein.py, implement a program in Python that prompts the user for mass as an integer (in kilograms) and then outputs the equivalent number of Joules as an integer. Assume that the user will input an integer.
+'''
